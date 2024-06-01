ca0b854a43ca012aff889fb074e4c4704e9afa37
Rafael Escobar Gutierrez <rafael.eg.code@gmail.com>
2024-05-31 21:32:46 -0600

    Agrega componente de modal generico

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -1,21 +1,29 @@
-<div>
-    <button id="btn-modal" class="focus:outline-none p-2 bg-blue-600 text-white bg-opacity-75 rounded-lg ring-4 ring-indigo-300" type="button">Open modal</button>
-</div>
+@props(['title', 'body', 'bottomButtons', 'openBtn'])
 
-<div id="modal"
-    class="fixed top-0 left-0 w-screen h-screen flex items-center justify-center bg-blue-500 bg-opacity-50 transform scale-0 transition-transform duration-300">
-    <div class="bg-white w-1/2 h-1/2 p-12">
-        <button id="close-btn" type="button" class="focus:outline-none">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
-                stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                    d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
-            </svg>
-        </button>
-        <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-            Minus placeat maiores repudiandae, error perferendis inventore
-            aspernatur voluptatum omnis sint debitis!
-        </p>
+<div>
+    <div>
+        <button id="btn-modal" class="focus:outline-none p-2 bg-blue-600 text-white bg-opacity-75 rounded-lg ring-4 ring-indigo-300" type="button">{{$openBtn}}</button>
+    </div>
+    <div id="modal" class="fixed top-0 left-0 w-screen h-screen flex items-center justify-center bg-gray-500 bg-opacity-50 transform scale-0 transition-transform duration-200">
+        <div class="h-4/6 w-2/6 rounded-xl bg-white">
+            <div class="flex h-[12%] items-center justify-between pl-6 border-b-2 border-gray-100">
+                <div>
+                    <span class="text-2xl font-medium">{{Str::limit($title, 30)}}</span>
+                </div>
+            </div>
+            <div class="h-[73%] px-6 py-4">
+                {{ $body }}
+            </div>
+            <div class="flex h-[15%] items-center justify-between border-t-2 border-gray-100 px-6 bg-gray-100 rounded-b-xl">
+                <div>
+                    <button id="close-btn" class="rounded-xl bg-cyan-500 px-4 py-2 text-center font-bold text-white hover:bg-cyan-300  active:bg-white active:text-blue-600 lg:px-3 lg:py-2">
+                        <span class="text-center text-xs md:text-sm 2xl:text-xl">Cerrar</span>
+                    </button>
+                </div>
+                <div class="flex gap-3">
+                    {{ $bottomButtons }}
+                </div>
+            </div>
+        </div>
     </div>
 </div>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,7 +11,23 @@
                 <x-welcome :sections="$sections" />
             </div>
         </div>
-        <x-modal />
+        <x-modal>
+            <x-slot name="title">Ejemplo de titulo</x-slot>
+            <x-slot name="body">
+                <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi quia nisi porro nihil, accusantium velit nemo vitae rem atque quidem voluptatem quo ullam, odit non eaque quaerat, deleniti ad ipsum!</span>
+            </x-slot>
+            <x-slot name="bottomButtons">
+                <button class="rounded-xl bg-green-500 px-4 py-2 text-center font-bold text-white hover:bg-green-50 hover:text-blue-400 active:bg-white active:text-blue-600 lg:px-3 lg:py-2">
+                <span class="text-center text-xs md:text-sm 2xl:text-xl">Añadir</span>
+                </button>
+                <button class="rounded-xl bg-blue-500 px-4 py-2 text-center font-bold text-white hover:bg-green-50 hover:text-blue-400 active:bg-white active:text-blue-600 lg:px-3 lg:py-2">
+                    <span class="text-center text-xs md:text-sm 2xl:text-xl">Guardar</span>
+                </button>
+            </x-slot>
+            <x-slot name="openBtn">
+                <span>Abrir</span>
+            </x-slot>
+        </x-modal>
         <x-floating-btn />
     </div>
 </x-app-layout>
