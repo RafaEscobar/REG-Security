cdfc9ae4b1e56fbc1a5b02d7926cd6754d71efdb
Rafael Escobar Gutierrez <rafael.eg.code@gmail.com>
2024-06-01 16:24:26 -0600

    Agrega ajustes visuales en el btn de guardado del formulario de creación de section

--- resources/views/modals/create-section.blade.php
+++ resources/views/modals/create-section.blade.php
@@ -4,7 +4,7 @@
        @include('models.sections.create')
     </x-slot>
     <x-slot name="bottomButtons">
-        <x-btn label="Guardar" class="bg-blue-600 text-white hover:bg-blue-400 active:bg-white active:text-black" id="create-section-submit"></x-btn>
+        <x-btn label="Guardar" class="bg-blue-700 text-white hover:bg-blue-500 active:bg-blue-300 active:text-white" id="create-section-submit"></x-btn>
     </x-slot>
     <x-slot name="openBtn">
         <x-floating-btn>
