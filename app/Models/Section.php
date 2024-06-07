1cf5e0481e703c6abf8f780071f28e4dc8eb268a
Rafael Escobar Gutierrez <rafael.eg.code@gmail.com>
2024-06-06 20:21:05 -0600

    Agrega configuración del modelo para usar MediaLibrary

--- app/Models/Section.php
+++ app/Models/Section.php
@@ -4,10 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\Image\Enums\Fit;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
-class Section extends Model
+class Section extends Model implements HasMedia
 {
-    use HasFactory;
+    use HasFactory, InteractsWithMedia;
 
     protected $fillable = [
         'name',
@@ -15,6 +19,14 @@ class Section extends Model
         'tag_id',
         'user_id'
     ];
+
+    public function registerMediaConversions(Media $media = null): void
+    {
+    $this
+        ->addMediaConversion('preview')
+        ->fit(Fit::Contain, 300, 300)
+        ->nonQueued();
+    }
 
     public function tag()
     {
