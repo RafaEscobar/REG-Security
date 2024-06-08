1b2d753312042b5e4131c3fc42e3b1202915bb7a
Rafael Escobar Gutierrez <rafael.eg.code@gmail.com>
2024-06-08 15:03:48 -0600

    Agrega funcionalidad completa del lanzamiento de las alertas

--- app/Http/Controllers/SectionController.php
+++ app/Http/Controllers/SectionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\SectionRequest;
 use App\Models\Section;
+use Error;
 
 class SectionController extends Controller
 {
@@ -14,21 +15,30 @@ class SectionController extends Controller
 
     public function store(SectionRequest $request)
     {
-        $section = Section::create(
-            array_merge(
-                $request->all(),
-                ['user_id' => Auth()->user()->id]
-            )
-        );
-        if (isset($request->imageSection)) {
-            $section->addMedia($request->file('imageSection')->getRealPath())->toMediaCollection('sections');
-        } else {
-            //* TODO: EN PRODUCCIONT DESCOMENTAR ESTO PARA QUE SE USEN LAS IMAGENES QUE SE TIENEN EN EL PROYECTO.
-            // $section->addMediaFromUrl(Storage::path('public/images/desk.jpg'))->toMediaCollection('sections');
-            $section->addMediaFromUrl('https://images.pexels.com/photos/796602/pexels-photo-796602.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')->toMediaCollection('sections');
+        try {
+            throw new Error('enbcfwebncjkewc');
+            $section = Section::create(
+                array_merge(
+                    $request->all(),
+                    ['user_id' => Auth()->user()->id]
+                )
+            );
+            if (isset($request->imageSection)) {
+                $section->addMedia($request->file('imageSection')->getRealPath())->toMediaCollection('sections');
+            } else {
+                //* TODO: EN PRODUCCIONT DESCOMENTAR ESTO PARA QUE SE USEN LAS IMAGENES QUE SE TIENEN EN EL PROYECTO.
+                // $section->addMediaFromUrl(Storage::path('public/images/desk.jpg'))->toMediaCollection('sections');
+                $section->addMediaFromUrl('https://images.pexels.com/photos/796602/pexels-photo-796602.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')->toMediaCollection('sections');
+            }
+            return redirect()->route('dashboard')->with([
+                'message' => 'Sección creada con exito',
+                'type' => 'success',
+            ]);
+        } catch (\Throwable $th) {
+            return redirect()->route('dashboard')->with([
+                'message' => 'Error al generar la sección, consulte el panel de control',
+                'type' => 'fail',
+            ]);
         }
-        $message = 'Por favor, completa tu perfil.';
-        $type = 'info';
-        return redirect()->route('dashboard')->with(compact('type', 'message'));
     }
 }
