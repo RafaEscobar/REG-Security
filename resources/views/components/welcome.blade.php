ade2d8f2ac1b7d9b9210c9e8869f20ab060765e9
Rafael Escobar Gutierrez <rafael.eg.code@gmail.com>
2024-06-08 22:23:49 -0600

    BreakPoint #1 - Intalación de lottie player -

--- resources/views/components/welcome.blade.php
+++ resources/views/components/welcome.blade.php
@@ -1,12 +1,18 @@
 @props(['sections'])
 
 <div>
-    <div class="grid grid-rows-4 grid-cols-3 gap-4 bg-white h-screen p-4">
-        @foreach ($sections as $section)
-            <x-section-card :title="$section->name" entriesCount="0" :tag="$section->tag->name" imgUrl="{{$section->getFirstMediaUrl('sections')}}" />
-        @endforeach
-    </div>
-    <div class="p-6">
-        {{ $sections->links() }}
-    </div>
+    @if(count($sections) > 0)
+        <div class="grid grid-rows-4 grid-cols-3 gap-4 bg-white h-screen p-4">
+            @foreach ($sections as $section)
+                <x-section-card :title="$section->name" entriesCount="0" :tag="$section->tag->name" imgUrl="{{$section->getFirstMediaUrl('sections')}}" />
+            @endforeach
+        </div>
+        <div class="p-6">
+            {{ $sections->links() }}
+        </div>
+    @else
+        <div class="bg-white h-screen p-4">
+            
+        </div>
+    @endif
 </div>
